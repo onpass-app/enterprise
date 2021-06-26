3fa4a583ebde09ba3ec73c2d976a7e905f6728d2
Anmol Brar <brarsanmol@protonmail.com>
2021-06-26 00:27:29 -0400

    Add QR Code overlay to QR Code scanner.

--- components/scanning/QRCodeScanner.tsx
+++ components/scanning/QRCodeScanner.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { useEffect } from "react"
 import { useState } from "react"
 import { StyleSheet } from "react-native"
+import { Div } from "react-native-magnus";
 import NoCameraPermissionMessage from "./NoCameraPermissionMessage"
 
 const QRCodeScanner = ({ shouldScan, setShouldScan }: { shouldScan: boolean, setShouldScan: any }) => {
@@ -22,12 +23,20 @@ const QRCodeScanner = ({ shouldScan, setShouldScan }: { shouldScan: boolean, set
             : <BarCodeScanner 
             type="back"
             barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-            style={StyleSheet.absoluteFillObject}
+            style={{ ...StyleSheet.absoluteFillObject, justifyContent: "center" } }
             onBarCodeScanned={!shouldScan ? undefined : ({ type, data }: { type: any, data: string }) => {
                 setShouldScan(false)
                 alert(`Scanned QR Code, Found type ${type} with data ${data}`)}
-            }
-            />}
+            }>
+                <Div
+                alignSelf="center"
+                borderWidth={10}
+                borderColor="yellow500"
+                borderStyle="dashed"
+                w={256}
+                h={256}
+                rounded="md" />
+            </BarCodeScanner>}
         </>
     )
 }  
